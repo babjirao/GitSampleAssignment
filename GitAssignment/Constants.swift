afc7bc4aa41a4869c68fb835c7833b611d176f3f
Babji Rao <babji.mca08@gmail.com>
2021-09-19 14:09:30 -0400

    Added constant swift file

---
+++ GitAssignment/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitAssignment
+//
+//  Created by Babji Rao Kajji (contractor) on 9/19/21.
+//  Copyright © 2021 Babji Rao Kajji. All rights reserved.
+//
+
+import Foundation
+
+class Constants: NSObject {
+    static let GitUrl = "https://api.github.com/repos/babjirao/GitSampleAssignment/commits";
+}
---
+++ GitAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitAssignment
+//
+//  Created by Babji Rao Kajji (contractor) on 9/19/21.
+//  Copyright © 2021 Babji Rao Kajji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
